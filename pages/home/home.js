a52005f7b0cb51ee7585434d380d2a69f8befddf
Clearner5682 <625293627@qq.com>
2020-04-16 20:50:18 +0800

    1、m-googitem样式调整，2、上拉加载更多数据，3、tab可以固定在顶部，4、增加回到m-backtop组件

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,5 @@
 const homeservice = require('../../services/homeservice')
-const types=['fashion','newStyle','chosen']
+const types = ['fashion', 'newStyle', 'chosen']
 
 Page({
   data: {
@@ -12,6 +12,9 @@ Page({
       { tabIndex: 2, name: "精选" }
     ],
     activeTabIndex: 0,
+    isShowBackTop: false,
+    tabToTop: 0,
+    isFixed: false,
     currentType: "fashion",
     tabGoods: {
       fashion: {
@@ -63,30 +66,75 @@ Page({
       .catch(res => { });
 
     // 拿tab页数据
-    this._getTabGoodData(0,1);
-    this._getTabGoodData(1,1);
-    this._getTabGoodData(2,1);
+    this._getTabGoodData(0, 1);
+    this._getTabGoodData(1, 1);
+    this._getTabGoodData(2, 1);
+  },
+  onShow() {
+    setTimeout(() => {
+      wx.createSelectorQuery().select('#mainTab').boundingClientRect(res => {
+        // console.log(res.top);
+        this.setData({
+          tabToTop: res.top
+        })
+      }).exec();
+    }, 1000)
+  },
+  onPageScroll(options) {
+    const isShowBackTop = options.scrollTop >= 1000;
+    if (isShowBackTop != this.data.isShowBackTop) {
+      this.setData({ isShowBackTop: isShowBackTop })
+    }
+    const isFixed = options.scrollTop > this.data.tabToTop;
+    if (this.data.isFixed != isFixed) {
+      this.setData({
+        isFixed: isFixed
+      })
+    }
+  },
+  onReachBottom() {
+    this.loadMore();
   },
   // 自定义方法
   tabClickHandler(event) {
-    this.setData({currentType:types[event.detail.activeIndex]});
+    this.setData({ currentType: types[event.detail.activeIndex] });
     this.setData({ activeTabIndex: event.detail.activeIndex });
   },
-  _getTabGoodData(tabIndex,page){
+  _getTabGoodData(tabIndex, page) {
     homeservice.GetTabGoodItem(tabIndex, page)
-        .then(res => {
-          const list = res.data.PagedList;
-          const total = res.data.total;
-          const type=types[tabIndex];
-          let tempData = this.data.tabGoods[type];
-          tempData.total = total;
-          tempData.goodItems.push(...list);
-          const dataKey="tabGoods."+type;
-          this.setData({
-            // ***注意这里必须加上[]
-            [dataKey]: tempData
-          })
+      .then(res => {
+        const list = res.data.PagedList;
+        const total = res.data.total;
+        const type = types[tabIndex];
+        let tempData = this.data.tabGoods[type];
+        tempData.page += 1;
+        tempData.total = total;
+        tempData.goodItems.push(...list);
+        const dataKey = "tabGoods." + type;
+        this.setData({
+          // ***注意这里必须加上[]
+          [dataKey]: tempData
         })
-        .catch(res => { });
+      })
+      .catch(res => { });
+  },
+  backtop() {
+    wx.pageScrollTo({
+      scrollTop: 0
+    })
+  },
+  loadMore() {
+    const activeTabIndex = this.data.activeTabIndex;
+    const tabGoodData = this.data.tabGoods[this.data.currentType];
+    if (tabGoodData.goodItems.length >= tabGoodData.total) {
+      wx.showToast({
+        title: '没有更多数据了',
+        icon: 'none',
+        duration: 1000
+      })
+      return;
+    }
+    const page = tabGoodData.page;
+    this._getTabGoodData(activeTabIndex, page);
   }
 })
